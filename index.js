3c74214ee0440b81e763e359b5f409c65b38ef34
aisha-jufe <aishajucfe287>
2022-11-23 09:06:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser='Grace Hopper';
+const welcomeMessage=`Welcome to Flatbook, ${currentUser} !`;
+const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`;
+const excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
